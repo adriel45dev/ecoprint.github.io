b4785ca94d6979e8086465440abe2d32a09c172c
Adriel Alves <98328671+adriel45dev@users.noreply.github.com>
2023-03-26 19:07:47 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const planters = [
-  { name: "Alice", treesPlanted: 5000 },
+  { name: "Alice", treesPlanted: 6000 },
   { name: "Bob", treesPlanted: 4500 },
   { name: "Carol", treesPlanted: 3500 },
   { name: "David", treesPlanted: 2000 },
